79b08b96c21fe7651c4121dc9535589c7588e2b1
rianssilva6-svg <rian.s.silva6@ba.estudante.senai.br>
2025-09-17 14:33:32 -0300

    Add files via upload

--- atividade 3 no streamlit.py
+++ atividade 3 no streamlit.py
@@ -2,16 +2,15 @@ import streamlit as st
 
 st.title("===login da conta===")
 
+
 login = st.text_input("Digite o login: ")
-senha = st.number_input("Digite a senha: ", step=5)
+senha = st.number_input("Digite a senha: ",step=1)
 
-st.button("Verificar")
-
-while True:
+if st.button("Verificar"):
     if login == "Rian" and senha == 1234:
-        st.success ("===Seja bem-vindo===")
-        break
+     st.success ("===Seja bem-vindo===")
     else:
-        st.error ("Login ou senha inválido!!")
-        break
+     st.error ("Login ou senha inválido!!")
+        
+
         
